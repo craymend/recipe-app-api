653e57f349580a85c6005193ded1bee93e271a31
Damien Row <damien@bdgwebdesign.com>
2022-05-22 20:57:03 -0700

    add wait for db tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,17 +5,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-        """Test the calc module."""
+    """Test the calc module."""
 
-        def test_add_numbers(self):
-            """Test adding numbers together."""
-            res = calc.add(5, 6)
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
 
-            self.assertEqual(res, 11)
-        
-        def test_subtract_numbers(self):
-            """Test adding numbers together."""
-            res = calc.subtract(5, 6)
-            
-            self.assertEqual(res, -1)
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """Test adding numbers together."""
+        res = calc.subtract(5, 6)
+
+        self.assertEqual(res, -1)
